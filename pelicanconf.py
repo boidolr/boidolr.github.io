02f5d32964d40becee2aa1138109dacd69648148
Raphael Boidol <boidolr@users.noreply.github.com>
2026-05-21 19:09:44 +0200

    feat: update listed packages

--- pelicanconf.py
+++ pelicanconf.py
@@ -39,7 +39,7 @@ JINJA_TESTS = {
     "repo_check": lambda repo_name: any(
         (
             part in repo_name
-            for part in ("actions", "cookiecutter", "pelican", "pre-commit", "pytest")
+            for part in ("ast-grep", "pelican", "pre-commit-images", "pre-commit-text", "pytest")
         )
     )
 }
